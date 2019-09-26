ac658feb747bc7eb75dee0c53fe5b267f5acc01a
AntonioChava96 <antonio.chava96@gmail.com>
2019-09-25 22:54:03 -0600

    Creacion del proyecto y modelo de ususarios

---
+++ webapi/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webapi.Models
+{
+    public class User
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string gender { get; set; }
+    }
+}
